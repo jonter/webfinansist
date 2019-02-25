8c248e2208a6e481dfa6a8fdbae9d04136bf3d80
jonter <jonterrusian@gmail.com>
2019-02-25 12:04:02 +0300

    Add style and model changes when click change-type-button

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,47 @@
-//TODO: make style changes when press changeTypeButton
+//TODO: Add data when click add-button
 
 const FinanceView = (function(){
     const DOMelements ={
         changeTypeButton:   document.querySelector(".js--button-change"),
         addItemButton:      document.querySelector(".js--button-add"),
         inputTitleItem:     document.querySelector(".js--input-description"),
-        inputValueItem:     document.querySelector(".js--input-value"),
-        
+        inputValueItem:     document.querySelector(".js--input-value") 
     }
     
     
+    function changeType(currentInputType){
+        
+        if(currentInputType==="income"){
+            const elements = document.querySelectorAll(".expense-color");
+            
+            for(let i=0;i<elements.length;i++ ){//fa-plus
+                elements[i].classList.remove("expense-color");
+                elements[i].classList.add("income-color");
+            }
+            
+            const icon = document.querySelector(".fa-minus");
+            icon.classList.remove("fa-minus");
+            icon.classList.add("fa-plus");
+            
+        }else{
+            const elements = document.querySelectorAll(".income-color");
+            
+            for(let i=0; i<elements.length; i++ ){
+                elements[i].classList.remove("income-color");
+                elements[i].classList.add("expense-color");
+                console.log('===expense');
+            }
+            
+            const icon = document.querySelector(".fa-plus");
+            icon.classList.remove("fa-plus");
+            icon.classList.add("fa-minus");
+        }
+        
+    }
     
     return{
-        DOMelements
+        DOMelements,
+        changeType
     }
     
     
@@ -21,6 +50,22 @@ const FinanceView = (function(){
 
 const FinanceModel = (function(){
     
+    const currentState = {
+        inputType: "income"
+    }
+    
+    
+    function changeType(){
+        if(currentState.inputType==="income")
+            currentState.inputType="expense";
+        else 
+            currentState.inputType="income";
+    }
+    
+    return{
+        currentState,
+        changeType
+    }
     
     
 })();
@@ -37,7 +82,16 @@ const FinanceController = (function(){
     
     
     function initialization(){
-        console.log("initialized");
+        
+        setupListeners();
+    }
+    
+    function setupListeners(){
+        DOMelements.changeTypeButton.addEventListener('click',()=>{
+            FinanceModel.changeType();
+            const currentInputType = FinanceModel.currentState.inputType;
+            FinanceView.changeType(currentInputType);  
+        });
     }
     
     return  {
