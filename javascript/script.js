fa3f6f550e6bec0b39718603ac0d1bc875ac7816
jonter <jonterrusian@gmail.com>
2019-02-26 07:28:18 +0300

    Add new item when press add-button or Enter
    Create a possibility to add item both to model and UI, but these features needs some changes such as percentage recalculations and updating total values

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,21 @@
-//TODO: Add data when click add-button
+//TODO: update total data, recalculate percentages when click add-button
 
 const FinanceView = (function(){
     const DOMelements ={
         changeTypeButton:   document.querySelector(".js--button-change"),
         addItemButton:      document.querySelector(".js--button-add"),
         inputTitleItem:     document.querySelector(".js--input-description"),
-        inputValueItem:     document.querySelector(".js--input-value") 
+        inputValueItem:     document.querySelector(".js--input-value"),
+        incomeContainer:    document.querySelector(".js--income-list"),
+        expenseContainer:   document.querySelector(".js--expense-list")
     }
-    
     
     function changeType(currentInputType){
         
         if(currentInputType==="income"){
             const elements = document.querySelectorAll(".expense-color");
             
-            for(let i=0;i<elements.length;i++ ){//fa-plus
+            for(let i=0; i < elements.length; i++){
                 elements[i].classList.remove("expense-color");
                 elements[i].classList.add("income-color");
             }
@@ -26,10 +27,9 @@ const FinanceView = (function(){
         }else{
             const elements = document.querySelectorAll(".income-color");
             
-            for(let i=0; i<elements.length; i++ ){
+            for(let i=0; i < elements.length; i++ ){
                 elements[i].classList.remove("income-color");
                 elements[i].classList.add("expense-color");
-                console.log('===expense');
             }
             
             const icon = document.querySelector(".fa-plus");
@@ -39,9 +39,69 @@ const FinanceView = (function(){
         
     }
     
+    function getUserInput(){
+        return {
+            value : DOMelements.inputValueItem.value,
+            description : DOMelements.inputTitleItem.value
+        }
+    }
+    
+    function addItem(newItem){
+        
+        let listElement, htmlCode;
+        
+        if(!newItem){
+            warnForInput();
+            return null;
+        }
+        
+        if(newItem.type==="income"){
+            listElement = DOMelements.incomeContainer;
+            htmlCode =  `
+                            <li class="income-item" id="item-${newItem.item.id}">
+                                <p class="item-description">${newItem.item.description}</p>
+                                <button class="delete-item-button"><i class="far fa-times-circle"></i></button>
+                                <p class="item-value">+ ${newItem.item.value} </p>
+                            </li>
+                        `;
+        }else{
+            listElement = DOMelements.expenseContainer;
+            let percentageHtml = newItem.item.percentage;
+            
+            if (percentageHtml === -1) {
+                percentageHtml = "&mdash;"; 
+            }
+            
+            htmlCode =  `
+                            <li class="expense-item" id="item-${newItem.item.id}">
+                                <p class="item-description">${newItem.item.description}</p>
+                                <button class="delete-item-button"><i class="far fa-times-circle"></i></button>
+                                <div class="item-percentage">${percentageHtml}</div>
+                                <p class="item-value">- ${newItem.item.value} </p>
+                            </li>
+                        `;
+        }
+        
+        
+        listElement.insertAdjacentHTML("afterbegin", htmlCode);
+        clearInput();
+    }
+        
+    function warnForInput(){
+        alert("you should enter description and value to add new item");
+    }
+    
+    function clearInput(){
+        DOMelements.inputValueItem.value = "";
+        DOMelements.inputTitleItem.value = "";
+        DOMelements.inputTitleItem.focus();   
+    }
+    
     return{
         DOMelements,
-        changeType
+        changeType,
+        getUserInput,
+        addItem
     }
     
     
@@ -54,6 +114,73 @@ const FinanceModel = (function(){
         inputType: "income"
     }
     
+    const data ={
+        totalValues:{
+            income:0,
+            expense:0,
+            profit:{
+                value:0,
+                percentage:0
+            }
+        },
+        
+        allItems:{
+            income:[],
+            expense:[]
+        }  
+    }
+    
+    const Income = function (id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+    
+    const Expense = function (id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
+        
+        this.calculatePercentage = function (totalIncome){
+            if(totalIncome - this.value > 0){
+                this.percentage = Math.round(this.value / totalIncome *100);
+            }else{
+                this.percentage = -1;
+            }
+        }
+    }
+    
+    function addItem(description, value){
+        
+        if(!description || !value){
+            return null;
+        }
+        
+        const itemsArray = data.allItems[currentState.inputType];
+        let id=0;
+        if(itemsArray.length>0){
+            const lastItem = itemsArray[itemsArray.length-1];
+            id = lastItem.id+1;
+        }
+        
+        let newItem;
+        if(currentState.inputType==="income"){
+            newItem = new Income(id, description, parseFloat(value));
+        }else{
+            newItem = new Expense(id, description, parseFloat(value));
+            newItem.calculatePercentage(data.totalValues.profit.value);
+        }
+        
+        data.allItems[currentState.inputType].push(newItem);
+        
+        return {
+            item: newItem,
+            type: currentState.inputType
+        };
+        
+    }
+    
     
     function changeType(){
         if(currentState.inputType==="income")
@@ -64,7 +191,8 @@ const FinanceModel = (function(){
     
     return{
         currentState,
-        changeType
+        changeType,
+        addItem
     }
     
     
@@ -92,6 +220,20 @@ const FinanceController = (function(){
             const currentInputType = FinanceModel.currentState.inputType;
             FinanceView.changeType(currentInputType);  
         });
+        
+        DOMelements.addItemButton.addEventListener('click',addItem);
+        document.addEventListener('keypress',function(event){
+            if (event.keyCode === 13 || event.which === 13) {
+                addItem();
+            }
+        })
+    }
+    
+    function addItem (){
+        const userInput = FinanceView.getUserInput();
+        const addedItem = FinanceModel.addItem(userInput.description,userInput.value);
+
+        FinanceView.addItem(addedItem);
     }
     
     return  {
