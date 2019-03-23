068714b14d50969b8e7d32ea86003dcbba3b9e6b
jonter <jonterrusian@gmail.com>
2019-03-23 18:49:46 +0300

    Update webpack configuration
    Separate config into 3 files, add copy plugin

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,73 @@
+//general setting for dev and build
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+const PATHS = { 
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../public'),
+  assets: 'assets'
+};
+
+module.exports = {
+  externals:{ 
+    paths: PATHS
+  },
+
+  entry: {
+    app: `${PATHS.src}/javascript` 
+  },
+  output: {
+    filename: `${PATHS.assets}/js/[name].js`, 
+    path: PATHS.dist, 
+    publicPath: '/' 
+  },
+  module: {
+    rules: [{         
+      test: /\.js$/,  
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    },
+    {        
+      test: /\.(png|jpg|git|svg)$/,  
+      loader: 'file-loader',
+      options:{
+        name:'[name].[ext]'
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { 
+            sourceMap: true, 
+            config: { path: `${PATHS.src}/javascript/configs/postcss.config.js` } 
+          }
+        }
+      ]
+    }]
+  },
+  
+  plugins: [
+    new MiniCssExtractPlugin({ 
+      filename: `${PATHS.assets}/css/[name].css`
+    }),
+    new HtmlWebpackPlugin({
+      hash: false, 
+      template: `${PATHS.src}/index.html`,
+      filename: './index.html'
+    }),
+    new CopyWebpackPlugin([
+      {from: `${PATHS.src}/img`, to:`${PATHS.assets}/img`},
+      {from: `${PATHS.src}/static`, to: ''}
+    ])
+  ],
+}
